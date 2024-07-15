5b40759a09aedba449d2a518d2dc8878cb9b3006
lkzm <lkzmkz@gmail.com>
2024-07-16 00:10:34 +0300

    sad

--- stream/stream/settings.py
+++ stream/stream/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'stream.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'streamdb',
+        'USER': 'lkz',
+        'PASSWORD': '502132',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
